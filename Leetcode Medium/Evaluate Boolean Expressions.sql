f73776ffb86b991cb702f63fe02840ef300ef6e1
Pravin Yadav <61359326+Pravin5959@users.noreply.github.com>
2023-08-12 21:25:16 +0530

    Update Evaluate Boolean Expressions.sql

--- Leetcode Medium/Evaluate Boolean Expressions.sql
+++ Leetcode Medium/Evaluate Boolean Expressions.sql
@@ -64,35 +64,38 @@
 -- | x            | =        | x             | true  |
 -- +--------------+----------+---------------+-------+
 -- As shown, you need find the value of each boolean exprssion in the table using the variables table.
-
-
+-- Explanation:
+-- Step1 : First we need to get the actual values for the operands which is present in value tables
+-- Step2 : As we need to get the values for both left and right operand we will have to merge the value table twice as using
+--         value tables once with both left and right operand won't result in correct behavior
+-- Step3 : Once we have the operand's value present based upon the operator given we will have to evaluate and provide the result
 
 
 WITH expression_extn AS (SELECT v.value AS left_val,
-								 left_operand
-									 operator,
-									 right_operand,
-									v.value AS right_val 
-							  FROM Expressions e
-							  INNER JOIN Variables v
-							  ON e.left_operand = v.name
-							  INNER JOIN Variables v1
-							  ON e.right_operand = v1.name),
+				left_operand
+				operator,
+				right_operand,
+				v1.value AS right_val 
+			 FROM Expressions e
+			 INNER JOIN Variables v
+			 ON e.left_operand = v.name
+			 INNER JOIN Variables v1
+			 ON e.right_operand = v1.name),
 
 
 pre_final AS (SELECT left_operand,
-					 operator,
-					 right_operand,
-					 CASE WHEN operator = '<' THEN left_val < right_val
-						  WHEN operator = '>' THEN left_val > right_val
-						  ELSE left_val = right_val
-					 END AS value
-			  FROM expression_extn)
+		     operator,
+		     right_operand,
+		     CASE WHEN operator = '<' THEN left_val < right_val
+			  WHEN operator = '>' THEN left_val > right_val
+			  ELSE left_val = right_val
+		     END AS value
+	      FROM expression_extn)
 
 SELECT left_operand,
-		operator,
-		right_operand,
-		CASE WHEN value = 1 then true
-			 ELSE false 
-		END AS value
-FROM pre_final
+       operator,
+       right_operand,
+       CASE WHEN value = 1 then true
+	    ELSE false 
+       END AS value
+FROM pre_final;
