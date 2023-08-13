e6dda8bdeb4f697dcbff09c4f253446a2e099992
Pravin Yadav <61359326+Pravin5959@users.noreply.github.com>
2023-08-13 17:45:35 +0530

    Create Find the start and end number of continuous ranges.sql

---
+++ Leetcode Medium/Find the start and end number of continuous ranges.sql
@@ -0,0 +1,56 @@
+-- Question 80
+-- Table: Logs
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | log_id        | int     |
+-- +---------------+---------+
+-- id is the primary key for this table.
+-- Each row of this table contains the ID in a log Table.
+
+-- Since some IDs have been removed from Logs. Write an SQL query to find the start and end number of continuous ranges in table Logs.
+
+-- Order the result table by start_id.
+
+-- The query result format is in the following example:
+
+-- Logs table:
+-- +------------+
+-- | log_id     |
+-- +------------+
+-- | 1          |
+-- | 2          |
+-- | 3          |
+-- | 7          |
+-- | 8          |
+-- | 10         |
+-- +------------+
+
+-- Result table:
+-- +------------+--------------+
+-- | start_id   | end_id       |
+-- +------------+--------------+
+-- | 1          | 3            |
+-- | 7          | 8            |
+-- | 10         | 10           |
+-- +------------+--------------+
+-- The result table should contain all ranges in table Logs.
+-- From 1 to 3 is contained in the table.
+-- From 4 to 6 is missing in the table
+-- From 7 to 8 is contained in the table.
+-- Number 9 is missing in the table.
+-- Number 10 is contained in the table.
+
+-- GOOD Question Pattern: To find continuity over a range in SQL
+-- Explabation :
+-- Step 1: For finding the continuity over a range in SQL we have to use row_numbers hel, as row number i always continuous subtracting it
+--         with the id will provide us group of continous values
+-- Step 2: Finally, we need to find the maximum and minimum value from that group
+
+WITH log_extn AS (SELECT log_id,
+                  		 (log_id - ROW_NUMBER() OVER()) AS log_grouper
+                  FROM Logs)
+SELECT MIN(log_id) AS start_id, MAX(log_id) AS end_id
+FROM log_extn
+GROUP BY log_grouper;
