f9db879f58a6a0c88d3e3e0c48a38271f1933236
Pravin Yadav <61359326+Pravin5959@users.noreply.github.com>
2023-08-15 22:10:17 +0530

    Update Highest grade for each student.sql

--- Leetcode Medium/Highest grade for each student.sql
+++ Leetcode Medium/Highest grade for each student.sql
@@ -41,13 +41,13 @@
 -- Step2 : Once the ordering is specified while applying dense rank, we can just take the 2nd record using the selection
 
 WITH enrollment_extn AS (SELECT student_id,
-	   							course_id,
-       							grade,
-       							DENSE_RANK() OVER(PARTITION BY student_id ORDER BY grade DESC,course_id) AS rnk
-						FROM Enrollments)
+	   			course_id,
+       				grade,
+       				DENSE_RANK() OVER(PARTITION BY student_id ORDER BY grade DESC,course_id) AS rnk
+			 FROM Enrollments)
 
 SELECT student_id,
-	   course_id,
+       course_id,
        grade
 FROM enrollment_extn
 WHERE rnk = 1;
