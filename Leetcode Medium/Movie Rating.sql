88f018233a758a167f2ec7f9a2cb08d900ad5041
Pravin Yadav <61359326+Pravin5959@users.noreply.github.com>
2023-08-17 09:16:14 +0530

    Create Movie Rating.sql

---
+++ Leetcode Medium/Movie Rating.sql
@@ -0,0 +1,113 @@
+-- Question 59
+-- Table: Movies
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | movie_id      | int     |
+-- | title         | varchar |
+-- +---------------+---------+
+-- movie_id is the primary key for this table.
+-- title is the name of the movie.
+-- Table: Users
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | user_id       | int     |
+-- | name          | varchar |
+-- +---------------+---------+
+-- user_id is the primary key for this table.
+-- Table: Movie_Rating
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | movie_id      | int     |
+-- | user_id       | int     |
+-- | rating        | int     |
+-- | created_at    | date    |
+-- +---------------+---------+
+-- (movie_id, user_id) is the primary key for this table.
+-- This table contains the rating of a movie by a user in their review.
+-- created_at is the user's review date. 
+ 
+
+-- Write the following SQL query:
+
+-- Find the name of the user who has rated the greatest number of the movies.
+-- In case of a tie, return lexicographically smaller user name.
+
+-- Find the movie name with the highest average rating in February 2020.
+-- In case of a tie, return lexicographically smaller movie name.
+
+-- Query is returned in 2 rows, the query result format is in the folowing example:
+
+-- Movies table:
+-- +-------------+--------------+
+-- | movie_id    |  title       |
+-- +-------------+--------------+
+-- | 1           | Avengers     |
+-- | 2           | Frozen 2     |
+-- | 3           | Joker        |
+-- +-------------+--------------+
+
+-- Users table:
+-- +-------------+--------------+
+-- | user_id     |  name        |
+-- +-------------+--------------+
+-- | 1           | Daniel       |
+-- | 2           | Monica       |
+-- | 3           | Maria        |
+-- | 4           | James        |
+-- +-------------+--------------+
+
+-- Movie_Rating table:
+-- +-------------+--------------+--------------+-------------+
+-- | movie_id    | user_id      | rating       | created_at  |
+-- +-------------+--------------+--------------+-------------+
+-- | 1           | 1            | 3            | 2020-01-12  |
+-- | 1           | 2            | 4            | 2020-02-11  |
+-- | 1           | 3            | 2            | 2020-02-12  |
+-- | 1           | 4            | 1            | 2020-01-01  |
+-- | 2           | 1            | 5            | 2020-02-17  | 
+-- | 2           | 2            | 2            | 2020-02-01  | 
+-- | 2           | 3            | 2            | 2020-03-01  |
+-- | 3           | 1            | 3            | 2020-02-22  | 
+-- | 3           | 2            | 4            | 2020-02-25  | 
+-- +-------------+--------------+--------------+-------------+
+
+-- Result table:
+-- +--------------+
+-- | results      |
+-- +--------------+
+-- | Daniel       |
+-- | Frozen 2     |
+-- +--------------+
+
+-- Daniel and Maria have rated 3 movies ("Avengers", "Frozen 2" and "Joker") but Daniel is smaller lexicographically.
+-- Frozen 2 and Joker have a rating average of 3.5 in February but Frozen 2 is smaller lexicographically.
+
+-- Explanation:
+-- Step1 : We need to find the users with maximum total number of ratings
+-- Step2 : We need to find the movies with highest average ratings across ratings given to them
+-- Step3 : We need to join both the query to there respective dimention tables to get the names
+
+WITH user_rating_extn AS (SELECT user_id,
+								 COUNT(movie_id) OVER(PARTITION BY user_id) AS user_rating_cnts
+						  FROM Movie_Rating),
+
+movie_rating_extn AS (SELECT movie_id,
+							 AVG(rating) OVER(PARTITION BY movie_id) AS avg_movie_rating
+					  FROM Movie_Rating
+					  WHERE SUBSTRING(created_at,1,7) = '2020-02')
+
+SELECT DISTINCT FIRST_VALUE(name) OVER (ORDER BY user_rating_cnts DESC, name ASC) AS results
+FROM Users u
+INNER JOIN user_rating_extn m
+ON u.user_id = m.user_id
+UNION ALL
+SELECT DISTINCT FIRST_VALUE(title) OVER (ORDER BY avg_movie_rating DESC, title ASC) AS results
+FROM Movies u
+INNER JOIN movie_rating_extn m
+ON u.movie_id = m.movie_id;
