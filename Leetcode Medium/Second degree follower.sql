d30e092c51eb26ca6fa3869f6c62a7b5693409ad
Pravin Yadav <61359326+Pravin5959@users.noreply.github.com>
2023-08-20 18:13:01 +0530

    Create Second degree follower.sql

---
+++ Leetcode Medium/Second degree follower.sql
@@ -0,0 +1,42 @@
+-- Question 70
+-- In facebook, there is a follow table with two columns: followee, follower.
+
+-- Please write a sql query to get the amount of each follower’s follower if he/she has one.
+
+-- For example:
+
+-- +-------------+------------+
+-- | followee    | follower   |
+-- +-------------+------------+
+-- |     A       |     B      |
+-- |     B       |     C      |
+-- |     B       |     D      |
+-- |     D       |     E      |
+-- +-------------+------------+
+-- should output:
+-- +-------------+------------+
+-- | follower    | num        |
+-- +-------------+------------+
+-- |     B       |  2         |
+-- |     D       |  1         |
+-- +-------------+------------+
+-- Explaination:
+-- Both B and D exist in the follower list, when as a followee, B's follower is C and D, and D's follower is E. A does not exist in follower list.
+ 
+
+-- Note:
+-- Followee would not follow himself/herself in all cases.
+-- Please display the result in follower's alphabet order.
+
+-- Explanation
+-- Step1 : We need to check if the people in follower section have ever followed anyone, if they have then we need to find there follower 
+--         counts
+-- Step2 : By doing left join we can retain the existing data and check the step1 condition and finally aggregate the data
+
+
+SELECT f1.followee AS follower, COUNT(DISTINCT f1.follower) AS num
+FROM Follow f1
+LEFT JOIN Follow f2
+ON f1.followee = f2.follower
+WHERE f2.followee is NOT NULL
+GROUP BY f1.followee;
