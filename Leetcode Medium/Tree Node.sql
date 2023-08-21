d685d6d6de0c5e97435a1219bf27dd77439f7614
Pravin Yadav <61359326+Pravin5959@users.noreply.github.com>
2023-08-21 11:32:26 +0530

    Update Tree Node.sql

--- Leetcode Medium/Tree Node.sql
+++ Leetcode Medium/Tree Node.sql
@@ -59,7 +59,7 @@
 SELECT DISTINCT t1.id,
 	   CASE WHEN t1.p_id IS NULL THEN 'Root'
        		WHEN t2.p_id IS NOT NULL THEN 'Inner'
-			WHEN t2.p_id IS NULL THEN 'Leaf'
+	  	WHEN t2.p_id IS NULL THEN 'Leaf'
        END AS Type
 FROM Tree t1
 LEFT JOIN Tree t2
